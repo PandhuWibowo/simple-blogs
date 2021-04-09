43ac1a42cd3f158200bd5351e3121bc6dbc666b3
Pandhu Wibowo <pandhuwibowo@Pandhus-MacBook-Pro.local>
2021-04-09 21:59:44 +0700

    add new project

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'id' => '28037a79-2bc0-4181-a65f-8f154f6de4c5',
+            'name' => 'General',
+            'short_description' => 'General category of post'
+        ]);
+    }
+}
